df860dd106ad2e6eee5dcc119ac34ee7dd58b9fc
Carles Muiños <carles.ml.dev@gmail.com>
2018-12-20 18:39:09 +0100

    update insomnia installer

--- insomnia/insomnia-install.sh
+++ insomnia/insomnia-install.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
 
 # Add to sources ...
-echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
-    | sudo tee -a /etc/apt/sources.list.d/insomnia.list
+# echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
+#     | sudo tee -a /etc/apt/sources.list.d/insomnia.list
 
 # Add public key used to verify code signature ...
-wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
-    | sudo apt-key add -
+# wget --quiet -O - https://insomnia.rest/keys/debian-public.key.asc \
+#     | sudo apt-key add -
 
 # Refresh repository sources and install Insomnia ...
-sudo apt update
-sudo apt install insomnia
+# sudo apt update
+# sudo apt install insomnia
 
+sudo snap install insomnia
 
 # vim:ft=sh
 # File: $DOTFILES_PATH/insomnia/insomnia-install.sh
