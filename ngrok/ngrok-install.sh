4240618280a5c0407dbb7a71995539d71e2699a6
Carles Muiños <carles.ml.dev@gmail.com>
2018-12-20 18:39:28 +0100

    update ngrok installer

--- ngrok/ngrok-install.sh
+++ ngrok/ngrok-install.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 
 # Download the latest stable version of ngrok ...
-[[ ! -d "${APPLICATIONS_PATH}/ngrok" ]] && mkdir -p ${APPLICATIONS_PATH}/ngrok
-curl -L https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o ${APPLICATIONS_PATH}/ngrok/ngrok.zip
+# [[ ! -d "${APPLICATIONS_PATH}/ngrok" ]] && mkdir -p ${APPLICATIONS_PATH}/ngrok
+# curl -L https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o ${APPLICATIONS_PATH}/ngrok/ngrok.zip
 
 # Install the latest version of ngrok ...
-unzip ${APPLICATIONS_PATH}/ngrok/ngrok.zip -d ${APPLICATIONS_PATH}/ngrok
-rm -f ${APPLICATIONS_PATH}/ngrok/ngrok.zip
+# unzip ${APPLICATIONS_PATH}/ngrok/ngrok.zip -d ${APPLICATIONS_PATH}/ngrok
+# rm -f ${APPLICATIONS_PATH}/ngrok/ngrok.zip
 
 # Apply executable permissions to the binary ...
-chmod +x ${APPLICATIONS_PATH}/ngrok/ngrok
+# chmod +x ${APPLICATIONS_PATH}/ngrok/ngrok
 
 # Add ngrok to your PATH
-ln -s ${APPLICATIONS_PATH}/ngrok/ngrok ${HOME}/.bin/ngrok
+# ln -s ${APPLICATIONS_PATH}/ngrok/ngrok ${HOME}/.bin/ngrok
 
+sudo snap install ngrok
 
 # vim:ft=sh
 # File: $DOTFILES_PATH/ngrok/ngrok-install.sh
