c66d34221ccc3444eb5e639070a52d052b4c3214
Carles Muiños <carles.ml.dev@gmail.com>
2018-12-20 18:38:48 +0100

    update docker installer

--- docker/docker-compose-install.sh
+++ docker/docker-compose-install.sh
@@ -2,14 +2,14 @@
 
 # Download the latest version of Docker Compose ...
 [[ ! -d "${APPLICATIONS_PATH}/docker-compose" ]] && mkdir -p ${APPLICATIONS_PATH}/docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o ${APPLICATIONS_PATH}/docker-compose/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o ${APPLICATIONS_PATH}/docker-compose/docker-compose
 ln -s ${APPLICATIONS_PATH}/docker-compose/docker-compose ${HOME}/.bin/docker-compose
 
 # Apply executable permissions to the binary ...
 chmod +x ${APPLICATIONS_PATH}/docker-compose/docker-compose
 
 # Install command completion for the zsh shell ...
-curl -L https://raw.githubusercontent.com/docker/compose/1.17.1/contrib/completion/zsh/_docker-compose > ${DOTFILES_PATH}/zsh/completion.d/_docker-compose
+curl -L https://raw.githubusercontent.com/docker/compose/1.23.1/contrib/completion/zsh/_docker-compose > ${DOTFILES_PATH}/zsh/completion.d/_docker-compose
 
 # Test the installation
 echo Test the installation running docker-compose --version
--- docker/docker-install.sh
+++ docker/docker-install.sh
@@ -1,26 +1,27 @@
 #!/usr/bin/env bash
 
 # Update the apt package index ...
-sudo apt update
+# sudo apt update
 
 # Install packages to allow apt to use a repository over HTTPS ...
-sudo apt install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+# sudo apt install \
+#     apt-transport-https \
+#     ca-certificates \
+#     curl \
+#     software-properties-common
 
 # Add Docker’s official GPG key ...
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+# curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Set up the stable repository ...
-echo deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable | sudo tee /etc/apt/sources.list.d/docker.list
+# echo deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable | sudo tee /etc/apt/sources.list.d/docker.list
 
 # Update the apt package index ...
-sudo apt update
+# sudo apt update
 
 # Install the latest version of Docker CE ...
-sudo apt install docker-ce
+# sudo apt install docker-ce
+sudo snap install docker
 
 # Create the docker group ...
 sudo groupadd docker
