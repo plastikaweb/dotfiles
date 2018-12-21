41ef1035896818d2a61831faef5f444194670e98
Carles Muiños <carles.ml.dev@gmail.com>
2018-12-21 18:48:10 +0100

    update n installer

--- n/n-install.sh
+++ n/n-install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-# Installing rbenv dependencies ...
-sudo apt install curl
+# Installing n dependencies ...
+sudo apt install -y curl make
 
 # Installing n, to change Node versions ...
 curl -L https://git.io/n-install | N_PREFIX=~/.n bash -s -- -y -n
